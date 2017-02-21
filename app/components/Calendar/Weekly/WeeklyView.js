f4944cb102e160f09d83954d99e255fe58dcdb44
korrybass <korry@penheadcreative.com>
2017-02-20 23:29:23 -0500

    added event positioning for weekly view

--- app/components/Calendar/Weekly/WeeklyView.js
+++ app/components/Calendar/Weekly/WeeklyView.js
@@ -4,15 +4,18 @@ import moment from 'moment';
 
 const mockTimes = [
     {
-        start: moment(),
-        end: moment().add(30, "minutes")
+        start: moment().add(27, 'minutes'),
+        end: moment().add(30, "minutes"),
+        title: "Time of schedule" 
+    },
+    {
+        start: moment().add(3, 'hours'),
+        end: moment().add(17, "minutes"),
+        title: "Second time"
     }
 ];
 
-console.log('mock times', mockTimes);
-
 export default class Week extends React.Component {
-
     constructor() {
         super();
         this.state = {
@@ -23,34 +26,50 @@ export default class Week extends React.Component {
         };
     }
 
+    calculateEventPositionToPixels (event) {
+        let startingHour = moment(event.start).hours();
+        let startingMinutes = moment(event.start).minutes();
+        let position = (startingHour * 60 + startingMinutes) * 0.7;
+        return position;
+    }
+
+    generateWeeklyEventElement (dayOfWeek){
+        return mockTimes.map((x, idx) => {
+            if(moment(x.start).day() === dayOfWeek){
+               return <div className={"rc-weekly-event"} key={idx} style={{top: this.calculateEventPositionToPixels(x) }}>
+                    <p className={"title"}>{x.title}</p>
+                </div>
+            }
+        })
+    };
+
     getPosition (xPos){
         let x = Math.floor(xPos / 1010 * 100);
         return x;
-        //return (x % 5) >= 5 ? parseInt(x / 5) * 5 + 5 : parseInt(x / 5) * 5
-        //return Math.ceil(percentage/5)*5;
-    }
+    };
 
     componentDidMount (){
-        window.addEventListener("resize", function () {
+        window.addEventListener("resize",  () => {
             console.log('resizing', this);
-        }.bind(this));
-    }
+        })
+    };
     buildWeeklyTimes (){
         return timeDivMap.map((x, idx) => {
             return <div className="rc-weekly-times" key={idx}><p className="rc-hourly-time"> {x}</p></div>;
         });
-    }
-
-    findEventPosition (hour){
-        
+    };
+    findEventPosition (hour){   
         return hourIndex * 21;
-    }
+    };
+
     buildWeeklySlots (){
         let timeDivs = [];
         for (let i = 1; i <= 24; i++){
-            timeDivs.push(<div key={i} className="rc-markercell">
-                <div className="rc-dualmarker"></div>
-            </div>);
+            timeDivs.push(
+                <div key={i} className="rc-markercell">
+                    <div className="rc-dualmarker"></div>
+                </div>
+            );
         }
         return timeDivs;
     }
@@ -59,20 +78,15 @@ export default class Week extends React.Component {
         for (let i = 1; i <= 7; i++){
             timeDivs.push(<td key={i} className="rc-weekly-day-col">
                 <div  ref={"day-column"+i} onClick={(e) => { this.onDayColClick(e, i)}} className="rc-col-eventwrapper" style={{height: "1008px", marginBottom: "-1008px"}}>
-                    <div>
-                        Test time
-                    </div>
+                   {this.generateWeeklyEventElement(i)}
                 </div>
             </td>);
         }
         return timeDivs;
     }
     onDayColClick (e, ref){
-
-        console.log(ref, this.refs);
         let eventPosY = e.clientY - e.target.offsetTop + this.refs['weeklyWrapper'].scrollTop;
         let elem = this.refs[ref];
-
         let nearestMultiple = Math.round(eventPosY / 21) * 21;
         let events = [...this.state.newEvents, {start: null, position: nearestMultiple}];
         this.setState({newEvents: events});
